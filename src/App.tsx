ddcbe851b72fa19d6a2c763cc11ff43dc0d2f5c2
Orewagenki <orewagenki3@gmail.com>
2024-12-14 09:26:14 -0700

    Installed ChakraUI

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
+import { Button } from "@chakra-ui/react";
+import { HStack } from "@chakra-ui/react";
+
 const App = () => {
-  return <div></div>;
+  return (
+    <HStack>
+      <Button>Click me</Button>
+      <Button>Click me</Button>
+    </HStack>
+  );
 };
 
 export default App;
