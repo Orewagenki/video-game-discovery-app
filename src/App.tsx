6640e9cb9b88606bf356f05e37362e95b4308238
Orewagenki <orewagenki3@gmail.com>
2024-12-15 09:16:27 -0700

    Build a responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import { Button } from "@chakra-ui/react";
-import { HStack } from "@chakra-ui/react";
+import { Grid, GridItem, Show } from "@chakra-ui/react";
 
-const App = () => {
+function App() {
   return (
-    <HStack>
-      <Button>Click me</Button>
-      <Button>Click me</Button>
-    </HStack>
+    <Grid
+      templateAreas={{
+        base: `"nav" "main"`,
+        lg: `"nav nav" "aside main"`,
+      }}
+    >
+      <GridItem area="nav" bg="coral">
+        Nav
+      </GridItem>
+      <Show above="lg">
+        <GridItem area="aside" bg="gold">
+          Aside
+        </GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">
+        Main
+      </GridItem>
+    </Grid>
   );
-};
+}
 
 export default App;
